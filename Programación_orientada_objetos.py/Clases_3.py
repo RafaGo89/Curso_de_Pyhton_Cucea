2d73510db933cc8b3dd3f0eb1428656eb1476d3c
Rafa Gómez <rafael.rodriguez7962@alumnos.udg.mx>
2023-06-14 20:56:39 -0600

    Aprendí acerca de la programación orientada a objetos

--- Programación_orientada_objetos.py/Clases_3.py
+++ Programación_orientada_objetos.py/Clases_3.py
@@ -1,17 +1,28 @@
 class Persona:
     def __init__(self, nombre, edad, status, carrera, codigo):
-        self._codigo = codigo
         self._nombre = nombre  #el guion bajo es para hacerlo privado
         self._edad = edad
         self._status = status
         self._carrera = carrera
+        self._codigo = codigo
         
-        
-    def saludo (self):
-        print (f"Hola, mi nombre es {self._nombre} y mi edad es {self._edad}")
+    def __str__(self):
+        return f"hola mi nombre es {self._nombre} y mi edad es {self._edad} persona"
+    
+class Estudiante (Persona):
+    def __init__ (self, nombre, edad, status, carrera, codigo, promedio, semestre):
+        super().__init__(nombre, edad, status, carrera, codigo)
+        self._promedio = promedio
+        self._semestre = semestre
 
-persona1 = Persona ("Rafa", 19)
-persona2 = Persona ("Joaquín", 15)
+    def __str__(self):
+        return f"Hola, mi nombre es {self._nombre} y mi edad es {self._edad} alumno"
 
-persona1.saludo ()
-persona2.saludo ()
+persona1 = Persona('raul',19, 'activo', 'licenciatura', 1234)
+alumno1 = Estudiante('raul2', 29, 'activo', 'licenciatura', 890, 89, 9)
+
+print(f'{persona1._codigo} {persona1._nombre}')
+print(f'{alumno1._codigo} {alumno1._nombre}')
+
+print(f'{persona1}')
+print(f'{alumno1}')
